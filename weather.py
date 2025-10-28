227e900e1b22850e941b4f37435401fa6ed81d0b
Philip Harman <43306247+philipharman@users.noreply.github.com>
2025-10-27 20:31:37 -0400

    Add 'Balmy!' to temperature print statement

--- weather.py
+++ weather.py
@@ -10,7 +10,7 @@ def fetch_weather(lat: float = 38.9, lon: float = -77.0):
         params=dict(latitude=lat, longitude=lon, hourly="temperature_2m"),
     )
     most_recent_temp = float(weather.json()["hourly"]["temperature_2m"][0])
-    print(f"Most recent temp C: {most_recent_temp} degrees")
+    print(f"Most recent temp C: {most_recent_temp} degrees. Balmy!")
     return
 
 
